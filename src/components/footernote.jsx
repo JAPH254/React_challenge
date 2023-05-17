d625481bc64b2b69984917a616c7653cd6e8b1f3
JAPH254 <omanyujapheth@gmail.com>
2023-05-17 11:04:05 +0300

    flex editing

--- src/components/footernote.jsx
+++ src/components/footernote.jsx
@@ -1,14 +1,21 @@
+
 function Footernote(){
     return(
         <div className="footer">
-        <footer >
-            <p>
+        <div >
+            <p className="para">
                 Stay connected, stay informed, and let Waplink be your guide in the ever-evolving world of technology.
-                                </p>
-                                <p>Waplink Got You Covered</p>
-                                <p>Senior @Omanyu Japheth</p>
-        </footer>
+            </p>
+                               
         </div>
+        <div> 
+            <p>Waplink Got You Covered</p>
+            <p>© Senior Omanyu Japheth</p>
+
+        </div>
+   
+        </div>
+        
     )
 }
 export default Footernote
